0b114a7712c1e2c0b1de5d73631904be53c229ed
Seb Pearce <sebpearce@gmail.com>
2019-08-30 13:35:10 +1000

    Continue remark plugin attempt (WIP)

--- site/plugins/gatsby-remark-containers/index.js
+++ site/plugins/gatsby-remark-containers/index.js
@@ -1,30 +1,62 @@
-// const unified = require("unified")
-// const parse = require("remark-parse")
-// const containers = require("remark-containers")
-// const stringify = require("rehype-stringify")
-// const remark2rehype = require("remark-rehype")
+"use strict"
 
-// const processor = unified()
-//   .use(parse)
-//   .use(containers, {
-//     default: true,
-//     custom: [
-//       {
-//         type: "aside",
-//         element: "article",
-//         transform: function(node, config, tokenize) {
-//           node.data.hProperties = {
-//             className: config || "testingonetwothree",
-//           }
-//         },
-//       },
-//     ],
-//   })
-//   .use(remark2rehype)
-//   .use(stringify)
+const parse = require("remark-parse")
+const containers = require("remark-containers")
+const unified = require("unified")
+const remark2rehype = require("remark-rehype")
+const stringify = require("rehype-stringify")
 
-module.exports = ({ markdownAST }, pluginOptions) => {
-  // Manipulate AST
-  console.log("KANGAROOS")
+const visit = require(`unist-util-visit`)
+const processor = unified()
+  // .use(parse)
+  .use(containers, {
+    default: true,
+    custom: [
+      {
+        type: "callout",
+        element: "article",
+        transform: function(node, config, tokenize) {
+          node.data.hProperties = {
+            className: config || "left",
+          }
+        },
+      },
+      {
+        type: "quote",
+        element: "aside",
+        transform: function(node, config, tokenize) {
+          var words = tokenizeWords.parse(config)
+
+          node.data.hProperties = {
+            className: `quoted ${words.shift()}`,
+          }
+          node.children.push({
+            type: "footer",
+            data: {
+              hName: "footer",
+            },
+            children: tokenize(words.join(" ")),
+          })
+        },
+      },
+    ],
+  })
+  .use(remark2rehype)
+  .use(stringify)
+
+module.exports = ({ markdownAST }, pluginOptions = {}) => {
+  // visit(markdownAST, `text`, node => {
+  //   const processedText = String(processor().processSync(node.value))
+  //   console.log(processedText)
+  //   node.value = processedText
+  // })
+  // return processor().processSync(parsedAST)
+  console.log(markdownAST)
   return markdownAST
 }
+
+// module.exports = ({ markdownAST }, pluginOptions) => {
+//   // Manipulate AST
+//   return processor.processSync(markdownAST)
+// }
+// module.exports = processor
